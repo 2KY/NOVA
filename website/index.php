5e27ef285df802e0e542f386063075ca93342c9a
2KY <skylarpaulreynolds@hotmail.com>
2013-05-25 17:56:43 -0300

    Create index.php

---
+++ website/index.php
@@ -0,0 +1,42 @@
+<html>
+    <head>
+		<title>THE NOVA PROJECT</title>
+		<link type="text/css" rel="stylesheet" href="internal/stylesheet.css"/>
+	</head>
+	
+	<body>
+	
+	<div class="header"><img src="media/logo.png"/></div>
+	
+	<div class="indexcontent" id="intent">
+	
+	<p id="indent">Welcome to <strong>NOVA</strong>! In order to make full use of our website, please login below.</p>
+	<form action="login.php" method="post">
+		Username: <input type="text" name="uname"><br>
+		Password: <input type="text" name="pass"><br>
+		<input type="submit">
+	</form>
+	<p id="smalltext">Don't have an account? <a href="register.php">Register</a> one today!</p>
+	<p id="smalltext">Forget your password? Click <a href="forgot.php">here</a> to have your password reset!</p>
+	
+	<div class="footer">
+		<p id="smalltext">Created by 2KY</p>
+	</div>
+	</div>
+	
+	<div class="pictures">
+		<table>
+			<tr></tr>
+			<td><img class="reduced" src="media/screens/samp01.png"/></td>
+			<td><img class="reduced" src="media/screens/samp02.png"/></td>
+			<td><img class="reduced" src="media/screens/samp03.png"/></td>
+			<tr></tr>
+			<td><img class="reduced" src="media/screens/samp04.png"/></td>
+			<td><img class="reduced" src="media/screens/samp05.png"/></td>
+			<td><img class="reduced" src="media/screens/samp06.png"/></td>
+			<tr>
+		</table>
+	</div>
+	
+	</body>
+</html>
