355ce62cbc579203fef57c46e714ebc1113a8ed6
2KY <skylarpaulreynolds@hotmail.com>
2013-05-25 18:02:13 -0300

    Created register2.php

---
+++ website/register2.php
@@ -0,0 +1,39 @@
+<html>
+    <head>
+		<title>The NOVA Project</title>
+		<link type="text/css" rel="stylesheet" href="internal/stylesheet.css"/>
+	</head>
+	
+	<body>
+	
+	<div class="header"><img src="media/logo.png"/></div>
+	
+	<div class="indexcontent" id="intent">
+	<p id="smalltext">Please enter a username (this will be your display name) and a secure password!</p>
+	<form action="register_actual.php" method="post">
+		Username: <input type="text" name="uname"><br>
+		Password: <input type="text" name="pass"><br>
+		<input type="submit">
+	</form>
+	
+	<div class="footer">
+		<p id="smalltext">Created by 2KY</p>
+	</div>
+	</div>
+	
+	<div class="pictures">
+		<table>
+			<tr></tr>
+			<td><img class="reduced" src="media/screens/samp01.png"/></td>
+			<td><img class="reduced" src="media/screens/samp02.png"/></td>
+			<td><img class="reduced" src="media/screens/samp03.png"/></td>
+			<tr></tr>
+			<td><img class="reduced" src="media/screens/samp04.png"/></td>
+			<td><img class="reduced" src="media/screens/samp05.png"/></td>
+			<td><img class="reduced" src="media/screens/samp06.png"/></td>
+			<tr>
+		</table>
+	</div>
+	
+	</body>
+</html>
