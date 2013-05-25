7f56309304078741bc008bd9a4bdfa453a3d2abe
2KY <skylarpaulreynolds@hotmail.com>
2013-05-25 18:00:51 -0300

    Created register.php
    Behaves as it should, just need to add more to the reasons why someone should register an account.

---
+++ website/register.php
@@ -0,0 +1,41 @@
+<html>
+    <head>
+		<title>NOVA | 2KY.US</title>
+		<link type="text/css" rel="stylesheet" href="internal/stylesheet.css"/>
+	</head>
+	
+	<body>
+	
+	<div class="header"><img src="media/logo.png"/></div>
+	
+	<div class="indexcontent" id="intent">
+	
+	<div class="lefttext"><h4>Creating an account at NOVA will give you access to:</h4>
+	<ol>
+		<li>Multiple gaming servers with an all-in-one highscore and statistics table!</li>
+	</ol>
+	
+	<h3><a href="register2.php">SIGN ME UP</a> | <a href="selfclosing.php">GET ME OUT OF HERE</a></h3>
+	</div>
+	
+	<div class="footer">
+		<p id="smalltext">Created by 2KY</p>
+	</div>
+	</div>
+	
+	<div class="pictures">
+		<table>
+			<tr></tr>
+			<td><img class="reduced" src="media/screens/samp01.png"/></td>
+			<td><img class="reduced" src="media/screens/samp02.png"/></td>
+			<td><img class="reduced" src="media/screens/samp03.png"/></td>
+			<tr></tr>
+			<td><img class="reduced" src="media/screens/samp04.png"/></td>
+			<td><img class="reduced" src="media/screens/samp05.png"/></td>
+			<td><img class="reduced" src="media/screens/samp06.png"/></td>
+			<tr>
+		</table>
+	</div>
+	
+	</body>
+</html>
