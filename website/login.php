740e75ea7d310b28a8636f03fe56c33505da4ffa
2KY <skylarpaulreynolds@hotmail.com>
2013-05-25 17:57:54 -0300

    Created login.php
    Further edits are needed; allows blank username & password if both are left blank.

---
+++ website/login.php
@@ -0,0 +1,50 @@
+<html>
+    <head>
+		<title>Welcome <?php echo $_POST['uname']; ?>!</title>
+		<link type="text/css" rel="stylesheet" href="internal/stylesheet.css"/>
+	</head>
+	
+	<?php
+		require_once("internal/connection.php");
+	?>
+	
+	<body>
+	
+		<?php
+			$username = $_POST['uname'];
+			$password = $_POST['pass'];
+			
+			if( !isset($username) )
+			{
+		?>
+				<script type="text/javascript">
+					alert( "You must enter a username!" ); 
+					history.back();
+				</script>
+		<?php
+			}
+			else if( !isset($password) )
+			{
+		?>
+			<script type="text/javascript">
+				alert( "You must enter a password!" );
+				history.back();
+			</script>
+		<?php
+			}
+			/*else if( !isset($username) && !isset($password) )
+			{
+		?>
+			<script type="text/javascript">
+				alert( "You can't leave the form blank!" );
+				history.back();
+			</script>
+		<?php
+			}*/
+		?>
+	
+		Welcome to NOVA, <?php echo $_POST['uname']; ?> <br>
+		Your password is <?php echo $_POST['pass']; ?> <br>
+		
+	</body>
+</html>
