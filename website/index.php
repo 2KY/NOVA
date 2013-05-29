8b33fbc0e8fb8255ee78346f7c50b0dd270010e8
Sky Reynolds <skylarpaulreynolds@hotmail.com>
2013-05-28 20:07:03 -0400

    Organized the webpage section, created a new design

--- website/index.php
+++ website/index.php
@@ -1,42 +1,63 @@
 <html>
     <head>
-		<title>THE NOVA PROJECT</title>
-		<link type="text/css" rel="stylesheet" href="internal/stylesheet.css"/>
+		<title>NOVA: Home</title>
+		<link rel="stylesheet" type="text/css" href="internal/stylesheet.css">
 	</head>
 	
-	<body>
+	<?php
+		require_once("internal/connection.php");
+	?>
 	
-	<div class="header"><img src="media/logo.png"/></div>
+	<body class="background">
 	
-	<div class="indexcontent" id="intent">
-	
-	<p id="indent">Welcome to <strong>NOVA</strong>! In order to make full use of our website, please login below.</p>
-	<form action="login.php" method="post">
-		Username: <input type="text" name="uname"><br>
-		Password: <input type="text" name="pass"><br>
-		<input type="submit">
-	</form>
-	<p id="smalltext">Don't have an account? <a href="register.php">Register</a> one today!</p>
-	<p id="smalltext">Forget your password? Click <a href="forgot.php">here</a> to have your password reset!</p>
-	
-	<div class="footer">
-		<p id="smalltext">Created by 2KY</p>
+	<div class="header">
+		<div id="headertext">NOVA</div>
+	</div>
+	<div class="navigation">
+		<div id="navigationitem">
+		<a href="index.php">HOME</a> <a href="index.php">PROJECT INFO</a> <a href="index.php">COMMUNITY</a>
 	</div>
 	</div>
-	
-	<div class="pictures">
-		<table>
-			<tr></tr>
-			<td><img class="reduced" src="media/screens/samp01.png"/></td>
-			<td><img class="reduced" src="media/screens/samp02.png"/></td>
-			<td><img class="reduced" src="media/screens/samp03.png"/></td>
-			<tr></tr>
-			<td><img class="reduced" src="media/screens/samp04.png"/></td>
-			<td><img class="reduced" src="media/screens/samp05.png"/></td>
-			<td><img class="reduced" src="media/screens/samp06.png"/></td>
-			<tr>
-		</table>
+	<div class="content">
+		<div id="serverarea">
+			<!-- Servers --!>
+			<div id="serverheader">
+				<strong>Active NOVA Servers</strong>
+			</div>
+			<div id="servername">
+				<div id="serverimage">
+					<img src="images/samp_logo.png" alt="San Andreas Multiplayer (www.sa-mp.com)"/>
+				</div>
+				<strong>NOVA Roleplay</strong>
+				<div id="serverstatus">
+					Server Status: Online
+				<div id="serverconnect">
+					(<a href="samp:127.0.0.1:7777">127.0.0.1:7777</a>)
+				</div>
+				</div>
+			</div>
+		</div>
+		<div id="accountarea">
+			<div id="accountheader">
+				<strong>Account Area</strong>
+			</div>
+			<div id="accountname">
+				<strong>The2KY</strong>
+			</div>
+			<div id="accountavatar">
+				<img src="images/wolfrun.gif"/>
+			</div>
+			<div id="accountinfo">
+				Last visited: 5/25/2013
+			</div>
+		</div>
+		<div id="eventarea">
+			<div id="eventheader">
+				<strong>NOVA Tournaments/Events</strong>
+			</div>
+			<!-- Event images are 790x100 --!>
+			<img src="images/activeevent.png"/>
+		</div>
 	</div>
-	
 	</body>
 </html>
